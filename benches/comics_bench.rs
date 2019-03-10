fa3f4f8595a63fc77e00aa63c7b16b349356270d
Kixiron <25047011+Kixiron@users.noreply.github.com>
2019-03-09 21:17:33 -0600

    Added benchmarks

---
+++ benches/comics_bench.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate criterion;
+extern crate rusty_xkcd;
+
+use criterion::Criterion;
+use rusty_xkcd::*;
+
+fn get_comic_bench(c: &mut Criterion) {
+    c.bench_function("Get Comic", |b| {
+        b.iter(|| Comic::get_comic(589).unwrap())
+    });
+}
+
+fn get_latest_comic_bench(c: &mut Criterion) {
+    c.bench_function("Get Comic", |b| {
+        b.iter(|| Comic::get_latest_comic().unwrap())
+    });
+}
+
+/* DISABLED UNTIL WORKING
+fn get_random_comic_bench(c: &mut Criterion) {
+    c.bench_function("Get Comic", |b| {
+        b.iter(|| Comic::get_random_comic().unwrap())
+    });
+}
+*/
+
+criterion_group!(comic_benches, get_comic_bench, get_latest_comic_bench /*, get_random_comic_bench*/);
+criterion_main!(comic_benches);
