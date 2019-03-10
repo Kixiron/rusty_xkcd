dbb32d354203b39d81ff1ff54aa03ba265ac51f8
Kixiron <25047011+Kixiron@users.noreply.github.com>
2019-03-09 22:09:45 -0600

    Fully disabled benchmarking

--- benches/comics_bench.rs
+++ benches/comics_bench.rs
@@ -1,3 +1,4 @@
+/*
 #[macro_use]
 extern crate criterion;
 extern crate rusty_xkcd;
@@ -13,16 +14,16 @@ fn get_latest_comic_bench(c: &mut Criterion) {
     c.bench_function("Get Latest Comic", |b| b.iter(|| Comic::get_latest_comic()));
 }
 
-/* DISABLED UNTIL WORKING
+DISABLED UNTIL WORKING
 fn get_random_comic_bench(c: &mut Criterion) {
     c.bench_function("Get Random Comic", |b| b.iter(|| Comic::get_random_comic()));
 }
-*/
+
 
 criterion_group!(comic_benches, get_comic_bench, get_latest_comic_bench);
 criterion_main!(comic_benches);
 
-/* Cargo.toml
+Cargo.toml
 [dev-dependencies]
 criterion = "0.2.10"
 
