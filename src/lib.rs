af3ef1ddc000fc970ac78ef883470566bc57dbd9
Kixiron <25047011+Kixiron@users.noreply.github.com>
2019-03-12 11:03:27 -0500

    License header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Copyright 2019 Kixiron
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! # Rusty Xkcd
 //!
 //! [![Crates.io](https://img.shields.io/crates/v/rusty_xkcd.svg)](https://crates.io/crates/rusty_xkcd)
